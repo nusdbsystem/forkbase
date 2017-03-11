04aadeff8d87b0eba5bea6ba9ed65253d53dfcd7
caiqc <caiqc@comp.nus.edu.sg>
2017-03-11 15:01:29 +0800

    add is_char_t and not_chart_t

--- include/utils/type_traits.h
+++ include/utils/type_traits.h
@@ -7,11 +7,40 @@
 
 namespace ustore {
 
+template <typename T>
+struct is_char{
+    constexpr static bool value = false;
+};
+
+template <>
+struct is_char<char>{
+    constexpr static bool value = true;
+};
+
+template <>
+struct is_char<signed char>{
+    constexpr static bool value = true;
+};
+
+template <>
+struct is_char<unsigned char>{
+    constexpr static bool value = true;
+};
+
 template <bool B, typename T>
   using enable_if_t = typename std::enable_if<B, T>::type;
 
 template <typename T>
   using is_integral_t = typename ustore::enable_if_t<std::is_integral<T>::value, T>;
+
+template <typename T>
+  using not_integral_t = typename ustore::enable_if_t<!std::is_integral<T>::value, T>;
+
+template <typename T>
+  using is_char_t = typename ustore::enable_if_t<std::is_char<T>::value, T>;
+
+template <typename T>
+  using not_char_t = typename ustore::enable_if_t<!std::is_char<T>::value, T>;
 
 }
 #endif
