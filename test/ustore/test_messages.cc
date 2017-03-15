b14cc5ba761e51cb1421731106cd38248d0b6d47
WANG Sheng <wangsheng1001@gmail.com>
2017-03-15 14:58:03 +0800

    Review protobuf message

--- test/ustore/test_messages.cc
+++ test/ustore/test_messages.cc
@@ -6,19 +6,18 @@
 
 using ustore::UStoreMessage;
 using ustore::byte_t;
-#define TEST_KEY_SIZE 32
-#define TEST_VERSION_SIZE 32
-#define TEST_BRANCH_SIZE 32
-#define TEST_VALUE_SIZE 128
+const size_t TEST_KEY_SIZE = 32;
+const size_t TEST_VERSION_SIZE = 32;
+const size_t TEST_BRANCH_SIZE = 32;
+const size_t TEST_VALUE_SIZE = 128;
 
-void random_vals(byte_t* buf, int size) {
+void randomVals(byte_t* buf, int size) {
   srand(time(NULL));
   for (int i = 0; i < size; i++)
-    buf[i] = rand() & 0xFF; 
+    buf[i] = rand() & 0xFF;
 }
 
-
-void populate_header(UStoreMessage *msg, UStoreMessage::Type type) {
+void populateHeader(UStoreMessage *msg, UStoreMessage::Type type) {
   msg->set_type(type);
 
   // generate random vals
@@ -26,9 +25,9 @@ void populate_header(UStoreMessage *msg, UStoreMessage::Type type) {
   byte_t version[TEST_VERSION_SIZE];
   byte_t branch[TEST_BRANCH_SIZE];
 
-  random_vals(key, TEST_KEY_SIZE);
-  random_vals(version, TEST_VERSION_SIZE);
-  random_vals(branch, TEST_BRANCH_SIZE);
+  randomVals(key, TEST_KEY_SIZE);
+  randomVals(version, TEST_VERSION_SIZE);
+  randomVals(branch, TEST_BRANCH_SIZE);
 
   // set fields
   msg->set_key(key, TEST_KEY_SIZE);
@@ -36,7 +35,7 @@ void populate_header(UStoreMessage *msg, UStoreMessage::Type type) {
   msg->set_branch(branch, TEST_BRANCH_SIZE);
 }
 
-bool check_payload(const byte_t* original, int size_original,
+bool checkPayload(const byte_t* original, int size_original,
                    const byte_t* recovered, int size_recovered) {
   if (size_original != size_recovered)
     return false;
@@ -48,19 +47,19 @@ bool check_payload(const byte_t* original, int size_original,
   return true;
 }
 
-TEST(TestMessage, Test_Put_Request) {
+TEST(TestMessage, TestPutRequest) {
   UStoreMessage msg;
-  populate_header(&msg, UStoreMessage::PUT_REQUEST);
+  populateHeader(&msg, UStoreMessage::PUT_REQUEST);
 
   // add payload
   UStoreMessage::PutRequestPayload *payload = msg.mutable_put_request_payload();
   byte_t value[TEST_VALUE_SIZE];
-  random_vals(value, TEST_VALUE_SIZE);
+  randomVals(value, TEST_VALUE_SIZE);
   payload->set_value(value, TEST_VALUE_SIZE);
 
   // serialized
   int msg_size = msg.ByteSize();
-  byte_t serialized[msg_size];
+  byte_t* serialized = new byte_t[msg_size];
   EXPECT_EQ(msg.SerializeToArray(serialized, msg_size), true);
 
   // deserialized
@@ -68,25 +67,27 @@ TEST(TestMessage, Test_Put_Request) {
   EXPECT_EQ(recovered_msg.ParseFromArray(serialized, msg_size), true);
   EXPECT_EQ(recovered_msg.type(), UStoreMessage::PUT_REQUEST);
   EXPECT_EQ(recovered_msg.has_put_request_payload(), true);
-  EXPECT_EQ(check_payload(value, TEST_VALUE_SIZE,
+  EXPECT_EQ(checkPayload(value, TEST_VALUE_SIZE,
         (const byte_t*)recovered_msg.put_request_payload().value().data(),
           recovered_msg.put_request_payload().value().length()), true);
+
+  delete[] serialized;
 }
 
-TEST(TestMessage, Test_Put_Response) {
+TEST(TestMessage, TestPutResponse) {
   UStoreMessage msg;
-  populate_header(&msg, UStoreMessage::PUT_RESPONSE);
+  populateHeader(&msg, UStoreMessage::PUT_RESPONSE);
 
   // add payload
-  UStoreMessage::PutResponsePayload *payload 
+  UStoreMessage::PutResponsePayload *payload
     = msg.mutable_put_response_payload();
   byte_t version[TEST_VERSION_SIZE];
-  random_vals(version, TEST_VERSION_SIZE);
+  randomVals(version, TEST_VERSION_SIZE);
   payload->set_new_version(version, TEST_VERSION_SIZE);
 
   // serialized
   int msg_size = msg.ByteSize();
-  byte_t serialized[msg_size];
+  byte_t* serialized = new byte_t[msg_size];
   EXPECT_EQ(msg.SerializeToArray(serialized, msg_size), true);
 
   // deserialized
@@ -94,20 +95,22 @@ TEST(TestMessage, Test_Put_Response) {
   EXPECT_EQ(recovered_msg.ParseFromArray(serialized, msg_size), true);
   EXPECT_EQ(recovered_msg.type(), UStoreMessage::PUT_RESPONSE);
   EXPECT_EQ(recovered_msg.has_put_response_payload(), true);
-  EXPECT_EQ(check_payload(version, TEST_VERSION_SIZE,
+  EXPECT_EQ(checkPayload(version, TEST_VERSION_SIZE,
      (const byte_t*)recovered_msg.put_response_payload().new_version().data(),
      recovered_msg.put_response_payload().new_version().length()), true);
+
+  delete[] serialized;
 }
 
-TEST(TestMessage, Test_Get_Request) {
+TEST(TestMessage, TestGetRequest) {
   UStoreMessage msg;
-  populate_header(&msg, UStoreMessage::GET_REQUEST);
+  populateHeader(&msg, UStoreMessage::GET_REQUEST);
 
   // no payload to add
 
   // serialized
   int msg_size = msg.ByteSize();
-  byte_t serialized[msg_size];
+  byte_t* serialized = new byte_t[msg_size];
   EXPECT_EQ(msg.SerializeToArray(serialized, msg_size), true);
 
   // deserialized
@@ -115,24 +118,25 @@ TEST(TestMessage, Test_Get_Request) {
   EXPECT_EQ(recovered_msg.ParseFromArray(serialized, msg_size), true);
   EXPECT_EQ(recovered_msg.type(), UStoreMessage::GET_REQUEST);
 
+  delete[] serialized;
 }
 
 
 // Get_Response is very similar to Put_Request
-TEST(TestMessage, Test_Get_Response) {
+TEST(TestMessage, TestGetResponse) {
   UStoreMessage msg;
-  populate_header(&msg, UStoreMessage::GET_RESPONSE);
+  populateHeader(&msg, UStoreMessage::GET_RESPONSE);
 
   // add payload
-  UStoreMessage::GetResponsePayload *payload 
+  UStoreMessage::GetResponsePayload *payload
     = msg.mutable_get_response_payload();
   byte_t value[TEST_VALUE_SIZE];
-  random_vals(value, TEST_VALUE_SIZE);
+  randomVals(value, TEST_VALUE_SIZE);
   payload->set_value(value, TEST_VALUE_SIZE);
 
   // serialized
   int msg_size = msg.ByteSize();
-  byte_t serialized[msg_size];
+  byte_t* serialized = new byte_t[msg_size];
   EXPECT_EQ(msg.SerializeToArray(serialized, msg_size), true);
 
   // deserialized
@@ -140,26 +144,27 @@ TEST(TestMessage, Test_Get_Response) {
   EXPECT_EQ(recovered_msg.ParseFromArray(serialized, msg_size), true);
   EXPECT_EQ(recovered_msg.type(), UStoreMessage::GET_RESPONSE);
   EXPECT_EQ(recovered_msg.has_get_response_payload(), true);
-  EXPECT_EQ(check_payload(value, TEST_VALUE_SIZE,
+  EXPECT_EQ(checkPayload(value, TEST_VALUE_SIZE,
         (const byte_t*)recovered_msg.get_response_payload().value().data(),
         recovered_msg.get_response_payload().value().length()), true);
 
+  delete[] serialized;
 }
 
-TEST(TestMessage, Test_Branch_Request) {
+TEST(TestMessage, TestBranchRequest) {
   UStoreMessage msg;
-  populate_header(&msg, UStoreMessage::BRANCH_REQUEST);
+  populateHeader(&msg, UStoreMessage::BRANCH_REQUEST);
 
   // add payload
-  UStoreMessage::BranchRequestPayload *payload 
+  UStoreMessage::BranchRequestPayload *payload
     = msg.mutable_branch_request_payload();
   byte_t branch[TEST_BRANCH_SIZE];
-  random_vals(branch, TEST_BRANCH_SIZE);
+  randomVals(branch, TEST_BRANCH_SIZE);
   payload->set_new_branch(branch, TEST_BRANCH_SIZE);
 
   // serialized
   int msg_size = msg.ByteSize();
-  byte_t serialized[msg_size];
+  byte_t* serialized = new byte_t[msg_size];
   EXPECT_EQ(msg.SerializeToArray(serialized, msg_size), true);
 
   // deserialized
@@ -167,23 +172,25 @@ TEST(TestMessage, Test_Branch_Request) {
   EXPECT_EQ(recovered_msg.ParseFromArray(serialized, msg_size), true);
   EXPECT_EQ(recovered_msg.type(), UStoreMessage::BRANCH_REQUEST);
   EXPECT_EQ(recovered_msg.has_branch_request_payload(), true);
-  EXPECT_EQ(check_payload(branch, TEST_BRANCH_SIZE,
+  EXPECT_EQ(checkPayload(branch, TEST_BRANCH_SIZE,
       (const byte_t*)recovered_msg.branch_request_payload().new_branch().data(),
       recovered_msg.branch_request_payload().new_branch().length()), true);
+
+  delete[] serialized;
 }
 
-TEST(TestMessage, Test_Branch_Response) {
+TEST(TestMessage, TestBranchResponse) {
   UStoreMessage msg;
-  populate_header(&msg, UStoreMessage::BRANCH_RESPONSE);
+  populateHeader(&msg, UStoreMessage::BRANCH_RESPONSE);
 
   // add payload
-  UStoreMessage::BranchResponsePayload *payload 
-    = msg.mutable_branch_response_payload(); 
+  UStoreMessage::BranchResponsePayload *payload
+    = msg.mutable_branch_response_payload();
   payload->set_status(false);
 
   // serialized
-  int msg_size = msg.ByteSize(); 
-  byte_t serialized[msg_size];
+  int msg_size = msg.ByteSize();
+  byte_t* serialized = new byte_t[msg_size];
   EXPECT_EQ(msg.SerializeToArray(serialized, msg_size), true);
 
   // deserialized
@@ -192,31 +199,33 @@ TEST(TestMessage, Test_Branch_Response) {
   EXPECT_EQ(recovered_msg.type(), UStoreMessage::BRANCH_RESPONSE);
   EXPECT_EQ(recovered_msg.has_branch_response_payload(), true);
   EXPECT_EQ(recovered_msg.branch_response_payload().status(), false);
+
+  delete[] serialized;
 }
 
 // MOVE_REQUEST and MOVE_RESPONSE are the same as BRANCH_REQUEST and RESPONSE
 
 
-TEST(TestMessage, Test_Merge_Request) {
+TEST(TestMessage, TestMergeRequest) {
   UStoreMessage msg;
-  populate_header(&msg, UStoreMessage::MERGE_REQUEST);
+  populateHeader(&msg, UStoreMessage::MERGE_REQUEST);
 
   // add payload
-  UStoreMessage::MergeRequestPayload *payload 
+  UStoreMessage::MergeRequestPayload *payload
     = msg.mutable_merge_request_payload();
   byte_t target_branch[TEST_BRANCH_SIZE];
-  random_vals(target_branch, TEST_BRANCH_SIZE);
+  randomVals(target_branch, TEST_BRANCH_SIZE);
   payload->set_target_branch(target_branch, TEST_BRANCH_SIZE);
   byte_t ref_branch[TEST_BRANCH_SIZE];
-  random_vals(ref_branch, TEST_BRANCH_SIZE);
+  randomVals(ref_branch, TEST_BRANCH_SIZE);
   payload->set_ref_branch(ref_branch, TEST_BRANCH_SIZE);
   byte_t value[TEST_VALUE_SIZE];
-  random_vals(value, TEST_VALUE_SIZE);
+  randomVals(value, TEST_VALUE_SIZE);
   payload->set_value(value, TEST_VALUE_SIZE);
 
   // serialized
   int msg_size = msg.ByteSize();
-  byte_t serialized[msg_size];
+  byte_t* serialized = new byte_t[msg_size];
   EXPECT_EQ(msg.SerializeToArray(serialized, msg_size), true);
 
   // deserialized
@@ -224,29 +233,31 @@ TEST(TestMessage, Test_Merge_Request) {
   EXPECT_EQ(recovered_msg.ParseFromArray(serialized, msg_size), true);
   EXPECT_EQ(recovered_msg.type(), UStoreMessage::MERGE_REQUEST);
   EXPECT_EQ(recovered_msg.has_merge_request_payload(), true);
-  EXPECT_EQ(check_payload(target_branch, TEST_BRANCH_SIZE,
+  EXPECT_EQ(checkPayload(target_branch, TEST_BRANCH_SIZE,
     (const byte_t*)recovered_msg.merge_request_payload().target_branch().data(),
     recovered_msg.merge_request_payload().target_branch().length()), true);
-  EXPECT_EQ(check_payload(ref_branch, TEST_BRANCH_SIZE,
+  EXPECT_EQ(checkPayload(ref_branch, TEST_BRANCH_SIZE,
     (const byte_t*)recovered_msg.merge_request_payload().ref_branch().data(),
     recovered_msg.merge_request_payload().ref_branch().length()), true);
-  EXPECT_EQ(check_payload(value, TEST_VALUE_SIZE,
+  EXPECT_EQ(checkPayload(value, TEST_VALUE_SIZE,
     (const byte_t*)recovered_msg.merge_request_payload().value().data(),
     recovered_msg.merge_request_payload().value().length()), true);
+
+  delete[] serialized;
 }
 
-TEST(TestMessage, Test_Merge_Response) {
+TEST(TestMessage, TestMergeResponse) {
   UStoreMessage msg;
-  populate_header(&msg, UStoreMessage::MERGE_RESPONSE);
+  populateHeader(&msg, UStoreMessage::MERGE_RESPONSE);
 
   // add payload
-  UStoreMessage::MergeResponsePayload *payload 
+  UStoreMessage::MergeResponsePayload *payload
     = msg.mutable_merge_response_payload();
   payload->set_status(true);
 
   // serialized
   int msg_size = msg.ByteSize();
-  byte_t serialized[msg_size];
+  byte_t* serialized = new byte_t[msg_size];
   EXPECT_EQ(msg.SerializeToArray(serialized, msg_size), true);
 
   // deserialized
@@ -255,5 +266,7 @@ TEST(TestMessage, Test_Merge_Response) {
   EXPECT_EQ(recovered_msg.type(), UStoreMessage::MERGE_RESPONSE);
   EXPECT_EQ(recovered_msg.has_merge_response_payload(), true);
   EXPECT_EQ(recovered_msg.merge_response_payload().status(), true);
+
+  delete[] serialized;
 }
 
