7e5fe7eef3a21ad4a5cb6646de9a9df8d9d9aaa6
WANG Sheng <wangsheng1001@gmail.com>
2017-04-19 20:41:36 +0800

    Use persist slice in container

--- src/worker/head_version.cc
+++ src/worker/head_version.cc
@@ -23,13 +23,39 @@ const std::unordered_set<Hash>& HeadVersion::GetLatest(const Slice& key) const {
 
 void HeadVersion::PutBranch(const Slice& key, const Slice& branch,
                             const Hash& ver) {
-  branch_ver_[Persist(key)][Persist(branch)] = ver.Clone();
+  auto key_it = branch_ver_.find(key);
+  // create key if not exists
+  if (key_it == branch_ver_.end()) {
+    branch_ver_.insert({PSlice::Persist(key),
+                       std::unordered_map<PSlice, Hash>()});
+    key_it = branch_ver_.find(key);
+    DCHECK(key_it != branch_ver_.end())
+      << "fail to insert new key into head table";
+  }
+  auto& branch_map = key_it->second; 
+  auto branch_it = branch_map.find(branch);
+  // create branch if not exists
+  if (branch_it == branch_map.end()) {
+    branch_map.insert({PSlice::Persist(branch), Hash()});
+    branch_it = branch_map.find(branch);
+    DCHECK(branch_it != branch_map.end())
+      << "fail to insert new branch into head table";
+  }
+  branch_it->second = ver.Clone();
   LogBranchUpdate(key, branch, ver);
 }
 
 void HeadVersion::PutLatest(const Slice& key, const Hash& prev_ver1,
                             const Hash& prev_ver2, const Hash& ver) {
-  auto& lv_key = latest_ver_[Persist(key)];
+  auto key_it = latest_ver_.find(key);
+  // create key is not exists
+  if (key_it == latest_ver_.end()) {
+    latest_ver_.insert({PSlice::Persist(key), std::unordered_set<Hash>()});
+    key_it = latest_ver_.find(key);
+    DCHECK(key_it != latest_ver_.end())
+      << "fail to insert new key into latest version table";
+  }
+  auto& lv_key = key_it->second;
   lv_key.erase(prev_ver1);
   lv_key.erase(prev_ver2);
   lv_key.insert(ver.Clone());
@@ -49,27 +75,29 @@ void HeadVersion::RemoveBranch(const Slice& key, const Slice& branch) {
 void HeadVersion::RenameBranch(const Slice& key, const Slice& old_branch,
                                const Slice& new_branch) {
   DCHECK(Exists(key, old_branch)) << ": Branch \"" << old_branch
-                                  << "for Key \"" << key
+                                  << "\" for Key \"" << key
                                   << "\" does not exist!";
   DCHECK(!Exists(key, new_branch)) << ": Branch \"" << new_branch
-                                   << "for Key \"" << key
+                                   << "\" for Key \"" << key
                                    << "\" already exists!";
   auto& bv_key = branch_ver_.at(key);
-  bv_key[Persist(new_branch)] = std::move(bv_key.at(old_branch));
+  bv_key[PSlice::Persist(new_branch)] = std::move(bv_key.at(old_branch));
   LogBranchUpdate(key, new_branch, bv_key.at(new_branch));
   bv_key.erase(old_branch);
   LogBranchUpdate(key, old_branch, Hash::kNull);
 }
 
 bool HeadVersion::Exists(const Slice& key, const Slice& branch) const {
-  if (branch_ver_.find(key) == branch_ver_.end()) return false;
-  const auto& bv_key = branch_ver_.at(key);
+  auto key_it = branch_ver_.find(key);
+  if (key_it == branch_ver_.end()) return false;
+  const auto& bv_key = key_it->second;
   return bv_key.find(branch) != bv_key.end();
 }
 
 bool HeadVersion::IsLatest(const Slice& key, const Hash& ver) const {
-  if (latest_ver_.find(key) == latest_ver_.end()) return false;
-  const auto& lv_key = latest_ver_.at(key);
+  auto key_it = latest_ver_.find(key);
+  if (key_it == latest_ver_.end()) return false;
+  const auto& lv_key = key_it->second;
   return lv_key.find(ver) != lv_key.end();
 }
 
@@ -82,16 +110,10 @@ std::unordered_set<Slice> HeadVersion::ListBranch(const Slice& key) const {
   std::unordered_set<Slice> branchs;
   if (branch_ver_.find(key) != branch_ver_.end()) {
     for (const auto& bv : branch_ver_.at(key)) {
-      branchs.insert(bv.first);
+      branchs.insert(bv.first.slice());
     }
   }
   return branchs;
-}
-
-Slice HeadVersion::Persist(const Slice& slice) {
-  std::string s(slice.data(), slice.len());
-  branch_str_.insert(s);
-  return Slice(*branch_str_.find(s));
 }
 
 }  // namespace ustore
