6647c06738bc7227563c84ea5831b07ff5521a7f
xiezl <xiezhongle@comp.nus.edu.sg>
2017-05-16 19:44:16 +0800

    modify start web server script

--- perfmon/script/start_webserver.sh
+++ perfmon/script/start_webserver.sh
@@ -6,6 +6,9 @@ bin=`cd $bin; pwd`
 PERFMON_HOME=$bin/../
 
 port=`grep "webserver_port=" $PERFMON_HOME/conf/perfmon.cfg | cut -d '=' -f 2`
+host=`grep "hostname=" $PERFMON_HOME/conf/perfmon.cfg | cut -d '=' -f 2`
 
+sed -e "6c\ \ Monitor.API=\"http://$host:$port\";" $PERFMON_HOME/webui/js/monitor.js
 cd $PERFMON_HOME/webui;
-python -m SimpleHTTPServer $port;
+
+#python -m SimpleHTTPServer $port;
