39c9210e3559668c1042782ef525205ccd01013b
WANG Sheng <wangsheng1001@gmail.com>
2017-06-03 17:37:11 +0800

    Reivew pr #152

--- tool/gen_poc_release.sh
+++ tool/gen_poc_release.sh
@@ -11,22 +11,22 @@ echo "mkdir -p $build_dir/poc_release/bin"
 mkdir -p $build_dir/poc_release/bin
 echo "mkdir -p $build_dir/poc_release/lib"
 mkdir -p $build_dir/poc_release/lib-suse
-echo "$build_dir/script/update_libs.sh $build_dir/bin/test_ustore $build_dir/poc_release/lib"
-$build_dir/script/update_libs.sh $build_dir/bin/test_ustore $build_dir/poc_release/lib
+echo "./tool/update_libs.sh $build_dir/bin/test_ustore $build_dir/poc_release/lib"
+./tool/update_libs.sh $build_dir/bin/test_ustore $build_dir/poc_release/lib
 echo "cp $build_dir/poc_release/lib/* $build_dir/poc_release/lib-suse"
 cp $build_dir/poc_release/lib/* $build_dir/poc_release/lib-suse
 echo "tar zxf $lib_archive -C $build_dir/poc_release"
 tar zxf $lib_archive -C $build_dir/poc_release
 echo "cp -f $build_dir/bin/* $build_dir/poc_release/bin/"
 cp -f $build_dir/bin/* $build_dir/poc_release/bin/
-#echo "cp -f $build_dir/lib/libustore.so $build_dir/poc_release/lib"
-#cp -f $build_dir/lib/libustore.so $build_dir/poc_release/lib
-#echo "cp -f $build_dir/lib/libustore.so $build_dir/poc_release/lib-suse"
-#cp -f $build_dir/lib/libustore.so $build_dir/poc_release/lib-suse
+# echo "cp -f $build_dir/lib/libustore.so $build_dir/poc_release/lib"
+# cp -f $build_dir/lib/libustore.so $build_dir/poc_release/lib
+# echo "cp -f $build_dir/lib/libustore.so $build_dir/poc_release/lib-suse"
+# cp -f $build_dir/lib/libustore.so $build_dir/poc_release/lib-suse
 echo "cp -r $build_dir/conf $build_dir/poc_release/conf"
 cp -r $build_dir/conf $build_dir/poc_release/conf
-echo "cp -r $build_dir/../perfmon $build_dir/poc_release/perfmon"
-cp -r $build_dir/../perfmon $build_dir/poc_release/perfmon
+# echo "cp -r $build_dir/../perfmon $build_dir/poc_release/perfmon"
+# cp -r $build_dir/../perfmon $build_dir/poc_release/perfmon
 echo "cp -r $build_dir/../doc $build_dir/poc_release/doc"
 cp -r $build_dir/../doc $build_dir/poc_release/doc
 echo "cp -r $build_dir/../webui $build_dir/poc_release/webui"
