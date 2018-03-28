bde5b6ba2585b7e7faa91017a7626577ef6b3206
WANG Sheng <wangsheng1001@gmail.com>
2018-03-28 21:02:27 +0800

    Add test script for http service

---
+++ bin/test_http_service.sh
@@ -0,0 +1,19 @@
+BASE_URL=http://127.0.0.1:60600
+# put
+echo [Put Key] ...
+curl $BASE_URL/put -X POST -d "key=http&branch=master&value=first"
+# list keys
+echo [List Keys] ...
+curl $BASE_URL/list -X GET
+# list branches
+echo [List Branches] ...
+curl $BASE_URL/list -X POST -d "key=http"
+# get
+echo [Get Key] ...
+curl $BASE_URL/get -X POST -d "key=http&branch=master"
+# get with xml format
+echo [Get Key with XML] ...
+curl $BASE_URL/get -X POST -d "key=http&branch=master" -H "accept: application/xml"
+# get with json format
+echo [Get Key with JSON] ...
+curl $BASE_URL/get -X POST -d "key=http&branch=master" -H "accept: application/json"
